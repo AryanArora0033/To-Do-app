d9bd2e79515e312eef47c707add0d4d7d48b9dc2
Aryan Arora <aryanarora0033@gmail.com>
2023-10-25 01:35:37 +0530

    Added comment

--- app/src/main/java/com/example/todo/Adapter.kt
+++ app/src/main/java/com/example/todo/Adapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.view.view.MyLayout
 import kotlinx.android.synthetic.main.view.view.priority
 import kotlinx.android.synthetic.main.view.view.title
+//nice work dude
 
 class Adapter(var data: List<cardinfo>) : RecyclerView.Adapter<Adapter.viewholder>() {
     class viewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
